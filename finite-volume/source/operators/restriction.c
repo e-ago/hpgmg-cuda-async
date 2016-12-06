0bd0517342d3a303f28de929408b05d3e1dee696
ec2-user <elena.ago@gmail.com>
2016-12-06 17:19:22 +0100

    fix

--- finite-volume/source/operators/restriction.c
+++ finite-volume/source/operators/restriction.c
@@ -274,12 +274,7 @@ void restriction_comm(level_type * level_c, int id_c, level_type *level_f, int i
     _timeStart = getTime();
     if(level_f->use_cuda) {
       cuda_restriction(*level_c,id_c,*level_f,id_f,level_f->restriction[restrictionType],restrictionType,0);
-      
       cudaDeviceSynchronize();  // switchover point: must synchronize GPU
-PUSH_RANGE("Comm flush", COMM_COL);
-//        cudaDeviceSynchronize();
-        comm_flush();
-POP_RANGE;
     }
     else {
       PRAGMA_THREAD_ACROSS_BLOCKS(level_f,buffer,level_f->restriction[restrictionType].num_blocks[0])
