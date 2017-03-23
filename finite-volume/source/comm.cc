14f9cb037ec024a3db22a9ec920ff4655a0adb9c
elenago <elena.ago@gmail.com>
2017-03-23 16:27:57 -0700

    debug

--- finite-volume/source/comm.cc
+++ finite-volume/source/comm.cc
@@ -199,7 +199,7 @@ int comm_init(MPI_Comm comm)
     bufSend = (char *) calloc(20, sizeof(char));
     memset(bufSend, 20, 'a');
     bufRecv = (char *) calloc(20, sizeof(char));
-    memset(bufRecv, 20, 0);
+    memset(bufRecv, 20, '0');
     
     comm_reg_t * send_buffers_reg;
     comm_reg_t * recv_buffers_reg;
