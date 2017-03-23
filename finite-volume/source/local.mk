1e9e38528c3a499ad1fa7ee0ef3d1530a27e6f49
elenago <elena.ago@gmail.com>
2017-03-23 10:59:06 -0700

    New HPGMG version

--- finite-volume/source/local.mk
+++ finite-volume/source/local.mk
@@ -1,4 +1,5 @@
 hpgmg-fv-y.c += $(call thisdir, \
+	debug.c \
 	timers.c \
 	level.c \
 	operators.fv4.c \
@@ -6,6 +7,11 @@ hpgmg-fv-y.c += $(call thisdir, \
 	solvers.c \
 	hpgmg-fv.c \
 	)
+
+hpgmg-fv-y.cc += $(call thisdir, \
+	comm.cc \
+	)
+
 hpgmg-fv-y.cu += $(call thisdir, \
 	cuda/operators.fv4.cu \
 	)
