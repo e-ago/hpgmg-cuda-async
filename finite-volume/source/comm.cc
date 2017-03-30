ebeb8b19ccedaea4d575ad37a8bc727b7f415ace
elenago <elena.ago@gmail.com>
2017-03-30 14:16:38 -0700

    Remove cudaFree from comm

--- finite-volume/source/comm.cc
+++ finite-volume/source/comm.cc
@@ -169,7 +169,7 @@ int comm_init(MPI_Comm comm)
     DBG("n_peers=%d\n", n_peers);
 
     //CUDA context initialization
-    cudaFree(0);
+    //cudaFree(0);
     mp_setup_gpu_id(current_gpu_id);
     MP_CHECK(mp_init(comm, peers, n_peers, MP_INIT_DEFAULT));
 
