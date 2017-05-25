3d084f5df2b83413dbdd5ef442e20b1e2304c85d
elenago <elena.ago@gmail.com>
2017-05-25 09:56:56 -0700

    Cleanup bigger test script

--- run_all_hpgmg.sh
+++ run_all_hpgmg.sh
@@ -18,32 +18,35 @@ do
         do
         	for var_proc in "${PROC[@]}"
         	do
-        		if [ $var_mode -eq 0 ]; then
-        			var_print_mode="MPI"
-        			var_comm=0;
-                        	var_async=0;
-                        	var_gpu=0;
-                        elif [ $var_mode -eq 1 ]; then
-                                var_print_mode="COMM"
-                                var_comm=1;
-                                var_async=0;
-                                var_gpu=0;
-        		elif [ $var_mode -eq 2 ]; then
-        			var_print_mode="ASYNC"		
-                                var_comm=1;
-                                var_async=1;
-                                var_gpu=0;
-        		else
-        			var_print_mode="GPU-initiated"
-                                var_comm=1;
-                                var_async=1;
-                                var_gpu=1;
-        		fi
-        		echo "MODE: $var_print_mode, SIZE: $var_size, PROC: $var_proc"
-        		file_out="hpgmg-$var_print_mode-s$var_size-p$var_proc.txt"
-        		./run.sh $var_proc $var_comm $var_async $var_gpu $var_size 8 &> $file_out
-        		egrep "use cuda" $file_out
-        		egrep "Total by level" $file_out
-        	done
+                        for num_iter in {1..5}
+                        do
+                		if [ $var_mode -eq 0 ]; then
+                			var_print_mode="MPI"
+                			var_comm=0;
+                                	var_async=0;
+                                	var_gpu=0;
+                                elif [ $var_mode -eq 1 ]; then
+                                        var_print_mode="COMM"
+                                        var_comm=1;
+                                        var_async=0;
+                                        var_gpu=0;
+                		elif [ $var_mode -eq 2 ]; then
+                			var_print_mode="ASYNC"		
+                                        var_comm=1;
+                                        var_async=1;
+                                        var_gpu=0;
+                		else
+                			var_print_mode="GPU-initiated"
+                                        var_comm=1;
+                                        var_async=1;
+                                        var_gpu=1;
+                		fi
+                		echo "MODE: $var_print_mode, SIZE: $var_size, PROC: $var_proc, ITER: $num_iter"
+                		file_out="hpgmg-$var_print_mode-s$var_size-p$var_proc.txt"
+                		./run.sh $var_proc $var_comm $var_async $var_gpu $var_size 8 &> $file_out
+                		egrep "use cuda" $file_out
+                		egrep "Total by level" $file_out
+                	done
+                done
         done
 done
