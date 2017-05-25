06cf94a13c8592ee0f9a471096b9cc748b2e3bd0
elenago <elena.ago@gmail.com>
2017-05-25 09:50:13 -0700

    Cleanup bigger test script

--- run_all_hpgmg.sh
+++ run_all_hpgmg.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #MPI PROC
 PROC=(2 4 8 16)
@@ -10,16 +10,10 @@ SIZE=(4 5 6 7)
 # 8^3  * 1 = 512
 # 4^3  * 1 = 64
 # 2^3 * 1 = 8
-THRESHOLD=10000
-var_threshold=10000
 MODE=(0 1 2 3) #MPI, COMM, ASYNC, GPU
-
-#EXCHANGE_HOST_ALLOC=1
-#EXCHANGE_MALLOC=0
 
 for var_mode in "${MODE[@]}"
 do
-        #Foreach size, try all threshold
         for var_size in "${SIZE[@]}"
         do
         	for var_proc in "${PROC[@]}"
@@ -48,10 +42,8 @@ do
         		echo "MODE: $var_print_mode, SIZE: $var_size, PROC: $var_proc"
         		file_out="hpgmg-$var_print_mode-s$var_size-p$var_proc.txt"
         		$PREFIX/src/hpgmg/run.sh $var_proc $var_comm $var_async $var_gpu $var_size 8 &> $file_out
-                        #$EXCHANGE_HOST_ALLOC $EXCHANGE_MALLOC $var_threshold
         		egrep "use cuda" $file_out
         		egrep "Total by level" $file_out
         	done
         done
-        #close mode
 done
