4f270b30243c5af9c77e7c55b633d4fc97653773
elenago <elenago@bj-f1-163-120.dyn.scz.us.nvidia.com>
2017-08-17 13:42:03 -0700

    Minor output fix

--- run_all_hpgmg.sh
+++ run_all_hpgmg.sh
@@ -43,8 +43,13 @@ do
                 		fi
                 		echo "MODE: $var_print_mode, SIZE: $var_size, PROC: $var_proc, ITER: $num_iter"
                 		file_out="hpgmg-$var_print_mode-s$var_size-p$var_proc.txt"
-                		./run.sh $var_proc $var_comm $var_async $var_gpu $var_size 8 &> $file_out
-                		egrep "use cuda" $file_out
+                                if [[ $num_iter -eq 1 ]]; then
+                                        ./run.sh $var_proc $var_comm $var_async $var_gpu $var_size 8 &> $file_out
+                                else
+                                        printf "\n\n===========================================================================\n\n" &>> $file_out
+                                        ./run.sh $var_proc $var_comm $var_async $var_gpu $var_size 8 &>> $file_out
+                		fi
+                                egrep "use cuda" $file_out
                 		egrep "Total by level" $file_out
                 	done
                 done
